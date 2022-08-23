db897183abcc0674de115bb566f243373d63e714
Andredex1911 <andredex1611@gmail.com>
2022-08-23 23:11:06 +0000

    asd

---
+++ idek.R
@@ -0,0 +1,11 @@
+Step 0: Owner and collaborator ensure all changes are updated
+Step 1: Owner makes a change and commits
+Step 2: Collaborator makes a change and commits on the same line
+Step 3: Collaborator pushes the file to GitHub
+Step 4: Owner pushes their changes and gets an error
+Step 5: Owner pulls from GitHub to get Collaborator changes
+Step 6: Owner edits the file to resolve the conflict
+Step 7: Owner commits the resolved changes
+Step 8: Owner pushes the resolved changes to GitHub
+Step 9: Collaborator pulls the resolved changes from GitHub
+Step 10: Both can view commit history
